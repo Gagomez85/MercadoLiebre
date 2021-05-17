4405d6457a0df4c95ce76c1ff392630bf1943b60
fer2021 <fernandorodriguez@smsconsulting.mobi>
2021-05-17 16:44:17 -0300

    mercadoliebre version 1.3

--- app.js
+++ app.js
@@ -6,9 +6,29 @@ console.log(publicPath) // nos da por consola la ruta del public
 
 app.use(express.static(publicPath));
 
-app.listen(3050, () => console.log('Servidor corriendo ')) // levantamos una server en localhost
+app.listen(3060, () => console.log('Servidor corriendo ')) // levantamos una server en localhost
 
 app.get('/', (req, res) => {
 
     res.sendFile(path.resolve(__dirname, './views/home.html'))
+});
+
+app.get('/login', function(req, res) {
+    let htmlPath = path.resolve(__dirname, './views/login.html')
+    res.sendFile(htmlPath)
+});
+
+app.get('/reset_account', function(req, res) {
+    let htmlPath = path.resolve(__dirname, './views/reset_account.html')
+    res.sendFile(htmlPath)
+});
+
+app.get('/register', function(req, res) {
+    let htmlPath = path.resolve(__dirname, './views/register.html')
+    res.sendFile(htmlPath)
+});
+
+app.get('/market', function(req, res) {
+    let htmlPath = path.resolve(__dirname, './views/market.html')
+    res.sendFile(htmlPath)
 });
